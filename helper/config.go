2cc4456d40d028b8f6be9810c94b6a35ccb7539a
Dewa Mahendra <31196162+mahendraputra21@users.noreply.github.com>
2022-05-04 18:38:09 +0800

    Update config.go

--- helper/config.go
+++ helper/config.go
@@ -13,7 +13,7 @@ import (
 //create connection with posgres
 func CreateConnection() *sql.DB {
 	//load .env file
-	err := godotenv.Load(os.ExpandEnv("E:/training/Go/catalsyt/go-rest-api/.env"))
+	err := godotenv.Load(os.ExpandEnv("go-rest-api/.env")) //please set path for the .env if you got the error message
 
 	if err != nil {
 		log.Fatalf("Error loading .env file")
